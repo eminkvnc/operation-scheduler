221b49348709df59bef7f181e826728f1b3314e0
eminkvnc <eminkvnc@gmail.com>
2020-12-15 23:37:44 +0300

    HomeScreen'e TabBar eklendi.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:operation_reminder/screens/home_operations_page.dart';
 
-class HomePage extends StatelessWidget {
+import 'home_drafts_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(vsync: this, length: 2, initialIndex: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColorLight,
+            child: TabBar(
+              controller: _tabController,
+              labelColor: Theme.of(context).primaryColor,
+              tabs: [
+                Tab(text: 'Drafts'),
+                Tab(text: 'Operations'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                HomeDraftsPage(),
+                HomeOperationsPage(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/viewmodel/home_model.dart
@@ -0,0 +1,3 @@
+import 'package:operation_reminder/viewmodel/base_model.dart';
+
+class HomeModel extends BaseModel {}
